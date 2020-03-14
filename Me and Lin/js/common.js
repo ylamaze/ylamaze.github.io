e56e7b3e7da407b0fe8d79129b61c248a20480d2
ylamaze <920765133@qq.com>
2020-03-14 14:34:55 +0800

    Site updated: 2020-03-14 14:34:54

--- Me and Lin/js/common.js
+++ Me and Lin/js/common.js
@@ -1,3 +1,11 @@
+<div id="txt">
+    <textarea  id="text" value="" cols="80" rows="10"></textarea><br>
+    <button  type="button" id="btn">评论留言</button>
+    <div id="lis">
+    <ol id="text_li">
+    </ol>
+    </div>
+    </div>
 var tet= document.getElementById("text");
 var butt= document.getElementById("btn");
 var tet_li= document.getElementById("text_li");
