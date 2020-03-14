064e821ebe5454b8c01e95be646c5b3451afcaf8
ylamaze <920765133@qq.com>
2020-03-14 13:28:56 +0800

    Site updated: 2020-03-14 13:28:51

---
+++ Me and Lin/js/common.js
@@ -0,0 +1,7 @@
+var tet= document.getElementById("text");
+var butt= document.getElementById("btn");
+var tet_li= document.getElementById("text_li");
+butt.onclick=function msg(){
+        var Li= document.createElement("li");
+        Li.innerHTML= tet.value;
+        tet_li.appendChild(Li);}
